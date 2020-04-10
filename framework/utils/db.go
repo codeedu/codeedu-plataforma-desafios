c2f0c942a68565421d8bcb4a360cf8b4899f6d95
Fabiano Leite <fabianoleittes@gmail.com>
2020-04-10 18:43:44 -0300

    Revert "fix: remove the extra error return"
    This reverts commit b7993a12d01ebef86711c9802cf9f386e65b1942.

--- framework/utils/db.go
+++ framework/utils/db.go
@@ -2,18 +2,21 @@ package utils
 
 import (
 	"fmt"
-	"log"
-	"os"
-
 	"github.com/codeedu/codeedu-plataforma-desafios/domain"
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func ConnectDB() *gorm.DB {
 
 	//Load environmenatal variables
 	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
